86b764a295e3c4f902d242a72e22e77d7bdefef3
richik2026 <richik_betting_top@outlook.com>
2026-09-09 14:10:39 +0300

    Add rating command templates

---
+++ backend/app/ratings/commands.py
@@ -0,0 +1,20 @@
+SALES_COMMANDS = {
+    "/top": "all",
+    "/toptoday": "today",
+    "/toptoweek": "week",
+    "/toptomonth": "month",
+    "/toptoyear": "year",
+}
+
+REELS_COMMANDS = {
+    "/reelstoptoday": "today",
+    "/reelstopweek": "week",
+    "/reelstopmonth": "month",
+    "/reelstopyear": "year",
+}
+
+SALES_GROUP_ID = 5476446888
+REELS_GROUP_ID = 1004405299682
+
+SALES_REPORT_TIMEZONE = "Europe/Moscow"
+REPORT_TIME = "23:59"
