ff3d6cf9529ddb6ec3a33568543f9c1605ab5b42
richik2026 <richik_betting_top@outlook.com>
2026-09-09 12:07:11 +0300

    Add telegram bot configuration with Tribute subscription link

---
+++ backend/app/telegram_bot/config.py
@@ -0,0 +1,8 @@
+TRIBUTE_SUBSCRIPTION_URL = "https://t.me/tribute/app?startapp=s12Ac"
+
+MAIN_MENU_ITEMS = [
+    "🦉 О проекте",
+    "🎙 Начать разговор",
+    "💳 Подписка",
+    "🆘 Техподдержка",
+]
